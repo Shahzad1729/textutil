9800b5d314d8d2ae0d2ab03a0aff7ef786b16d64
Shahzad Sayyed <98218245+Shahzad1729@users.noreply.github.com>
2022-06-03 17:58:55 +0530

    Add files via upload

---
+++ Textutils Website/textutils/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def removePunc(text):
+    punctuations = '''!@#$%^&*():"",.?/][}{\|=+-_~`'''
+    analyzedText = ""
+    for char in text:
+        if char not in punctuations:
+            analyzedText = analyzedText+char
+    return analyzedText
+
+
+def toLower(text):
+    analyzedText = text
+    analyzedText = analyzedText.lower()
+    return analyzedText
+
+
+def toUpper(text):
+    analyzedText = text
+    analyzedText = analyzedText.upper()
+    return analyzedText
+
+
+def Analyze(request):
+    Text = request.POST.get('text', 'default')
+    removepunc = request.GET.get('removepunc', 'off')
+    lowercase = request.GET.get('lowercase', 'off')
+    uppercase = request.GET.get('uppercase', 'off')
+
+    if (removepunc == 'off' and lowercase == "off" and uppercase == "off") or ( removepunc == 'on' and lowercase == "on" and uppercase == "on"):
+            return HttpResponse("Error")
+
+    elif (removepunc == 'on' and uppercase == 'on'):
+        result = removePunc(Text)
+        result = toUpper(result)
+        params = {'purpose': 'Punctuations & UpperCase', 'analyzetext': result}
+        return render(request, 'analyze.html', params)
+
+    elif (removepunc == 'on' and lowercase == 'on'):
+        result = removePunc(Text)
+        result = toLower(result)
+        params = {'purpose': 'Punctuations & LowerCase', 'analyzetext': result}
+        return render(request, 'analyze.html', params)
+
+
+    elif removepunc == 'on':
+        result = removePunc(Text)
+        params = {'purpose': 'Remove Punctuations', 'analyzetext': result}
+        return render(request, 'analyze.html', params)
+    elif lowercase == "on":
+        result = toLower(Text)
+        params = {'purpose': 'To LowerCase', 'analyzetext': result}
+        return render(request, 'analyze.html', params)
+    elif uppercase == "on":
+        result = toLower(Text)
+        params = {'purpose': 'To UpperCase', 'analyzetext': result}
+        return render(request, 'analyze.html', params)
+    else:
+        pass
